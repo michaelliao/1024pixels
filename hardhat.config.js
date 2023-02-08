ce04b0ac702ec0b291c194501adca2b8b270034d
Michael Liao <askxuefeng@gmail.com>
2023-02-08 22:13:46 +0800

    add api exporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-abi-exporter');
 
 module.exports = {
     solidity: "0.8.17",
+    abiExporter: {
+        path: "./abi",
+        clear: false,
+        flat: true,
+        only: [
+            "Pixels",
+            "Animations"
+        ],
+        pretty: false,
+        runOnCompile: true,
+    },
     mocha: {
         timeout: 10000000
     }
