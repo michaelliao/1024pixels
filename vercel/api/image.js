b987c563634cdc4783c6c833e00edfe0c34acbeb
Crypto Michael <askxuefeng@gmail.com>
2023-10-21 21:06:00 +0800

    Update image.js

--- vercel/api/image.js
+++ vercel/api/image.js
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
+const { VercelRequest, VercelResponse } = require('@vercel/node');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const url = 'https://api.thegraph.com/subgraphs/name/michaelliao/1024pixels';
   const id = req.query.id;
   if (!id) {
